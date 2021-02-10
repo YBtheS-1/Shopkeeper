eb1553d90d2039a3f2209758a19756d2b538dfa7
YBtheS-1 <449000p@gmail.com>
2021-02-10 14:54:38 -0500

    Added log statement

--- bot.js
+++ bot.js
@@ -25,5 +25,5 @@ client.on('message', message => {
  
 
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+console.log(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
